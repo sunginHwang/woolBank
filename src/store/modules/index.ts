7a3edb0983a73b185e0a1de620bb1be7ea6efd52
hwangseong-in <gommpo111@gmail.com>
2020-06-14 21:03:11 +0900

    setup: fix reducer load

--- src/store/modules/index.ts
+++ src/store/modules/index.ts
@@ -1,12 +1,10 @@
-import {combineReducers} from 'redux';
-
 // @ts-ignore
 const requireModule: any = require.context('.', true, /^(?!.\/index).*.ts$/);
 
 const modules: any = {};
 requireModule.keys().forEach((filename: string) => {
-    const moduleName: string = filename.replace(/(\.\/|\.ts)/g, '');
-    modules[moduleName] = requireModule(filename).default;
+  const moduleName: string = filename.replace(/(\.\/|\.ts)/g, '');
+  modules[moduleName] = requireModule(filename).default.reducer;
 });
 
-export default combineReducers(modules);
+export default modules;
