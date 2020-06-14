37d49a446ed90626c15fbfa71ca77f75de368195
hwangseong-in <gommpo111@gmail.com>
2020-06-14 21:03:56 +0900

    setup: add main Route

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import {Route, Switch} from 'react-router';
+import { Route, Switch } from 'react-router';
 import loadable from '@loadable/component';
 
-const todo = loadable(() => import('../pages/Todo'))
+const todo = loadable(() => import('../pages/Todo'));
+const main = loadable(() => import('../pages/Main'));
 
 function Routes() {
 
-    return (
-        <Switch>
-            <Route path="/todo" component={todo} exact/>
-        </Switch>
-    );
+  return (
+    <Switch>
+      <Route path="/" component={main} exact/>
+      <Route path="/todo" component={todo} exact/>
+    </Switch>
+  );
 };
 
 export default Routes;
