e69fedd0287f99e660613c2379b4bcca0ffe8ea2
hwangseong-in <gommpo111@gmail.com>
2020-06-25 22:22:30 +0900

    feat: 적금 타입 스타일 정의

--- src/components/wallet/addPhase/WalletTypeAddModal.tsx
+++ src/components/wallet/addPhase/WalletTypeAddModal.tsx
@@ -19,6 +19,9 @@ function WalletTypeAddModal({
   return (
     <ModalDeem visible={visible} onDeemClick={oncloseModal}>
       <S.WalletTypeAddModal visible={visible}>
+        <S.Title>
+          <p>작성하실 예/적금 종류를 선택해주세요.</p>
+        </S.Title>
         {assetTypes.map((assetType) => {
           return (
             <p
@@ -36,6 +39,7 @@ function WalletTypeAddModal({
 
 const S: {
   WalletTypeAddModal: any;
+  Title: any;
 } = {
   WalletTypeAddModal: styled.div`
     position: fixed;
@@ -49,9 +53,29 @@ const S: {
     z-index: ${(props) => props.theme.zIndex.modalDeem + 1};
     box-shadow: 0.1rem 0.3rem 1rem 0.2rem rgba(0, 0, 0, 0.2);
 
-    p {
-      height: 15rem;
+    >p {
+      margin-left: 1rem;
+      padding: 1.4rem;
+      font-size: 1.8rem;
+      font-weight: 600;
+      color: ${(props) => props.theme.colors.blackL1};
+      text-align: left;
     }
+    
+    >p:last-child{
+        margin-bottom: 2.5rem;
+    }
+  `,
+  Title: styled.div`
+    padding: 2rem;
+    display: flex;
+    justify-content: center;
+      p {
+        font-size: 1.6rem;
+        font-weight: 600;
+       
+        color: ${(props) => props.theme.colors.blackL1};
+      } 
   `
 };
 
