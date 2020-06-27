88ec4f1725516f7da36f8db62c95cbcf70d01222
hwangseong-in <gommpo111@gmail.com>
2020-06-27 16:40:45 +0900

    feat: 공용 백버튼 헤더 컴포넌트 작성

---
+++ src/components/common/HeaderWithBack.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import theme from '../../style/colors';
+import IcoChevronLeft from '../icon/IcoChevronLeft';
+
+type HeaderWithBackProps = {
+  title: string;
+  onBackClick: () => void;
+};
+
+
+function HeaderWithBack({ title, onBackClick }: HeaderWithBackProps) {
+  return (
+    <S.HeaderWithBack>
+      <div onClick={onBackClick}>
+        <IcoChevronLeft width={26} height={26} fill={theme.colors.navyD1}/>
+        <p>{title}</p>
+      </div>
+    </S.HeaderWithBack>
+  );
+}
+
+const S: any = {
+  HeaderWithBack: styled.div`
+    height: 5.5rem;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding-left: 1rem;
+    background-color: ${(props) => props.theme.colors.white};
+    
+    div {
+      display: flex;
+      flex-direction: row;
+    }
+    
+    p {
+      font-size: 1.6rem;
+      margin-top: 0.4rem;
+      color: ${(props) => props.theme.colors.blackL1};
+    }
+  `
+};
+
+export default HeaderWithBack;
