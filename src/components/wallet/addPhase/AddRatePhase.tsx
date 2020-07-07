2c9f11a4dcbaf629fbfcda42da3d110cd3268c87
sunginHwang <gommpo111@gmail.com>
2020-07-07 19:01:07 +0900

    feat: 적금 이율 계산기 작업 중

--- src/components/wallet/addPhase/AddRatePhase.tsx
+++ src/components/wallet/addPhase/AddRatePhase.tsx
@@ -3,27 +3,48 @@ import styled from 'styled-components';
 import PhaseTemplate from '../../common/PhaseTemplate';
 import HeaderWithBack from '../../common/HeaderWithBack';
 import BaseSlider from '../../common/BaseSlider';
+import { addComma } from '../../../support/util/String';
+import { IWalletForm } from '../../../models/IWalletForm';
+import { diffMonth } from '../../../support/util/date';
 
 type AddRatePhaseProps = {
   isActivePhase: boolean;
-  rate: number;
+  wallet: IWalletForm;
   goPrevPhase: () => void;
+  onChangeWalletForm: (type: string, value: number) => void;
 };
 
 function AddRatePhase({
-                        isActivePhase,
-                        rate,
-                        goPrevPhase
-                      }: AddRatePhaseProps) {
+  isActivePhase,
+  wallet,
+  goPrevPhase,
+  onChangeWalletForm
+}: AddRatePhaseProps) {
+  const onChangeRate = (e: React.ChangeEvent<HTMLInputElement>) =>
+    onChangeWalletForm('rate', Number(e.target.value));
+
+  const diffWalletMonth = diffMonth(new Date(), new Date(wallet.date));
+  console.log(diffWalletMonth);
+  console.log(wallet.rate * 0.1);
+  const futureRateAmount = wallet.amount * diffWalletMonth * ((diffWalletMonth + 1) / 2) * (wallet.rate * 0.1 / 12);
 
   return (
     <PhaseTemplate active={isActivePhase}>
-      <HeaderWithBack title='이율 설'
-                      onBackClick={goPrevPhase}/>
+      <HeaderWithBack title='이율 설정' onBackClick={goPrevPhase} />
       <S.AddRatePhase>
-        <p>연: {rate} <span>%</span></p>
         <div>
-          <BaseSlider min={0} max={100} value={rate} step={1}/>
+          <S.Header>
+            <p>적금의 이율을 설정해 주세요.</p>
+          </S.Header>
+          <div>
+            <BaseSlider min={1} max={100} value={wallet.rate} step={1} onChange={onChangeRate} />
+          </div>
+          <S.Amount>
+            <p>
+              예상 만기 이자 : <span>{addComma(futureRateAmount)}원</span>
+            </p>
+          </S.Amount>
+          <S.CompleteButton>이율 설정</S.CompleteButton>
         </div>
       </S.AddRatePhase>
     </PhaseTemplate>
@@ -32,23 +53,57 @@ function AddRatePhase({
 
 const S: {
   AddRatePhase: any;
+  Header: any;
+  Amount: any;
+  CompleteButton: any;
 } = {
   AddRatePhase: styled.div`
     justify-content: space-between;
     background-color: ${(props) => props.theme.colors.white};
     font-size: 1.8rem;
     height: 100%;
-    
-    >p {
+
+    > div {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      margin: 0 2rem;
+    }
+  `,
+  Header: styled.div`
+    padding-top: 2rem;
+    margin-bottom: 10rem;
+
+    > p {
       font-size: 2.4rem;
       font-weight: bold;
       margin-bottom: 5rem;
-      color: ${(props) => props.theme.colors.navyD1};
+      color: ${(props) => props.theme.colors.blackL1};
     }
-    
-    >div{
-      margin: 0 2rem;
+  `,
+  Amount: styled.div`
+    margin-top: 4rem;
+
+    > p {
+      font-size: 1.6rem;
+      font-weight: bold;
+      margin-bottom: 5rem;
+      color: ${(props) => props.theme.colors.blackL1};
+
+      > span {
+        color: ${(props) => props.theme.colors.navyD1};
+      }
     }
+  `,
+  CompleteButton: styled.button`
+    margin-top: auto;
+    margin-bottom: 7.5rem;
+    height: 5.5rem;
+    width: 100%;
+    border-radius: 0.8rem;
+    color: ${(props) => props.theme.colors.white};
+    background-color: ${(props) => props.theme.colors.navyD1};
+    opacity: ${(props: any) => (props.active ? 1 : 0.5)};
   `
 };
 
