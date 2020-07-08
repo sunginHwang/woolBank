fd07087762353b57a5b9bfd76d0623bab75f8ce5
hwangseong-in <gommpo111@gmail.com>
2020-07-08 23:09:30 +0900

    feat: 토글 아웃라인 추가 옵션 설정

--- src/components/common/ToggleTab.tsx
+++ src/components/common/ToggleTab.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 type ToggleTabProps = {
   tabs: string[];
+  useOutline?: boolean;
   activeTab: string;
   onChangeTab: (tab: string) => void;
 };
@@ -10,18 +11,32 @@ type ToggleTabProps = {
 function ToggleTab({
                      tabs,
                      activeTab,
+                     useOutline = true,
                      onChangeTab
                    }: ToggleTabProps) {
+
   return (
-    <S.ToggleTab>
-      {
-        tabs.map((tab, index) => {
+    <S.ToggleTab useOutline={useOutline}>
+      { // 라인 없는 탭 구조
+        !useOutline && tabs.map((tab, index) => {
           return (
             <S.Tab key={index}
                    active={tab === activeTab}
-                   onClick={() => onChangeTab(tab)}
-              >{tab}</S.Tab>
-          )
+                   onClick={() => onChangeTab(tab)}>
+              {tab}
+            </S.Tab>
+          );
+        })
+      }
+      { // 아웃라인 탭 구조
+        useOutline && tabs.map((tab, index) => {
+          return (
+            <S.TabOutLine key={index}
+                          active={tab === activeTab}
+                          onClick={() => onChangeTab(tab)}>
+              {tab}
+            </S.TabOutLine>
+          );
         })
       }
     </S.ToggleTab>
@@ -30,19 +45,27 @@ function ToggleTab({
 
 const S: {
   ToggleTab: any;
-  Tab: any
+  Tab: any;
+  TabOutLine: any;
 } = {
   ToggleTab: styled.div`
     width: 100%;
-    height: 5rem;
+    height: ${(props: any) => props.useOutline ? '4' : '5'}rem;
     display: flex;
     justify-content: space-around;
     margin-bottom: 1rem;
   `,
-  Tab : styled.button`
+  Tab: styled.button`
     width: 100%;
+    border: .1rem solid ${(props: any) => props.active ? props.theme.colors.navyD1 : props.theme.colors.greyL1};
     color: ${(props: any) => props.active ? props.theme.colors.navyD1 : props.theme.colors.blackL1};
-    border-bottom: ${(props: any) => props.active ? '.2rem': '.1rem'} solid ${(props: any) => props.active ? props.theme.colors.navyD1: props.theme.colors.greyL1};
+    border-bottom: ${(props: any) => props.active ? '.2rem' : '.1rem'} solid ${(props: any) => props.active ? props.theme.colors.navyD1 : props.theme.colors.greyL1};
+  `,
+  TabOutLine: styled.button`
+    width: 100%;
+    border: .1rem solid ${(props: any) => props.active ? props.theme.colors.navyD1 : props.theme.colors.greyL6};
+    color: ${(props: any) => props.active ? props.theme.colors.navyD1 : props.theme.colors.greyL6};
+   
   `
 };
 
