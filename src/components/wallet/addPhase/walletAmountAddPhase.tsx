efbe34835c5e00cbd53bf7aa8555152f537a41aa
sunginHwang <gommpo111@gmail.com>
2020-07-09 19:02:35 +0900

    feat: 금액 세팅 유효성 체크

--- src/components/wallet/addPhase/walletAmountAddPhase.tsx
+++ src/components/wallet/addPhase/walletAmountAddPhase.tsx
@@ -11,17 +11,28 @@ type WalletAmountAddPhaseProps = {
   onChangeWalletForm: (type: string, value: number) => void;
 };
 
-function WalletAmountAddPhase({ amount, isActivePhase, onChangeWalletForm, goPrevPhase, goNextPhase }: WalletAmountAddPhaseProps) {
-  const onChangeAmount = (num: number) => onChangeWalletForm('amount', num);
+function WalletAmountAddPhase({
+  amount,
+  isActivePhase,
+  onChangeWalletForm,
+  goPrevPhase,
+  goNextPhase
+}: WalletAmountAddPhaseProps) {
   const isActiveComplete = amount > 0;
+
+  const onChangeAmount = (num: number) => onChangeWalletForm('amount', num);
+
+  const onCompleteClick = () => isActiveComplete && goNextPhase();
+
   return (
     <PhaseTemplate active={isActivePhase}>
-      <HeaderWithBack title='예/적금액 작성'
-                      onBackClick={goPrevPhase}/>
-      <NumberInput currentAmount={amount}
-                   isActiveComplete={isActiveComplete}
-                   onCompleteClick={goNextPhase}
-                   onChangeAmount={onChangeAmount}/>
+      <HeaderWithBack title='예/적금액 작성' onBackClick={goPrevPhase} />
+      <NumberInput
+        currentAmount={amount}
+        isActiveComplete={isActiveComplete}
+        onCompleteClick={onCompleteClick}
+        onChangeAmount={onChangeAmount}
+      />
     </PhaseTemplate>
   );
 }
