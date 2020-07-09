92205007e1e963470340efd4509c5a288f3bf679
sunginHwang <gommpo111@gmail.com>
2020-07-09 18:54:28 +0900

    feat: 토글 탭 구조 변경

--- src/components/common/ToggleTab.tsx
+++ src/components/common/ToggleTab.tsx
@@ -1,47 +1,55 @@
 import React from 'react';
 import styled from 'styled-components';
+import { IAssetType } from '../../models/IAssetType';
 
 type ToggleTabProps = {
-  tabs: string[];
+  tabs: IAssetType[];
   useOutline?: boolean;
-  activeTab: string;
-  onChangeTab: (tab: string) => void;
+  activeTab: IAssetType;
+  onChangeTab: (tab: IAssetType) => void;
 };
 
 function ToggleTab({
-                     tabs,
-                     activeTab,
-                     useOutline = true,
-                     onChangeTab
-                   }: ToggleTabProps) {
-
+  tabs,
+  activeTab,
+  useOutline = true,
+  onChangeTab
+}: ToggleTabProps) {
   return (
     <S.ToggleTab useOutline={useOutline}>
-      { // 라인 없는 탭 구조
-        !useOutline && tabs.map((tab, index) => {
-          return (
-            <S.Tab key={index}
-                   active={tab === activeTab}
-                   onClick={() => onChangeTab(tab)}>
-              {tab}
-            </S.Tab>
-          );
-        })
+      {
+        // 라인 없는 탭 구조
+        !useOutline &&
+          tabs.map((tab, index) => {
+            return (
+              <S.Tab
+                key={tab.type}
+                active={tab.name === activeTab.name}
+                onClick={() => onChangeTab(tab)}
+              >
+                {tab.name}
+              </S.Tab>
+            );
+          })
       }
-      { // 아웃라인 탭 구조
-        useOutline && tabs.map((tab, index) => {
-          return (
-            <S.TabOutLine key={index}
-                          active={tab === activeTab}
-                          onClick={() => onChangeTab(tab)}>
-              {tab}
-            </S.TabOutLine>
-          );
-        })
+      {
+        // 아웃라인 탭 구조
+        useOutline &&
+          tabs.map((tab) => {
+            return (
+              <S.TabOutLine
+                key={tab.type}
+                active={tab.name === activeTab.name}
+                onClick={() => onChangeTab(tab)}
+              >
+                {tab.name}
+              </S.TabOutLine>
+            );
+          })
       }
     </S.ToggleTab>
   );
-};
+}
 
 const S: {
   ToggleTab: any;
@@ -50,22 +58,29 @@ const S: {
 } = {
   ToggleTab: styled.div`
     width: 100%;
-    height: ${(props: any) => props.useOutline ? '4' : '5'}rem;
+    height: ${(props: any) => (props.useOutline ? '4' : '5')}rem;
     display: flex;
     justify-content: space-around;
     margin-bottom: 1rem;
   `,
   Tab: styled.button`
     width: 100%;
-    border: .1rem solid ${(props: any) => props.active ? props.theme.colors.navyD1 : props.theme.colors.greyL1};
-    color: ${(props: any) => props.active ? props.theme.colors.navyD1 : props.theme.colors.blackL1};
-    border-bottom: ${(props: any) => props.active ? '.2rem' : '.1rem'} solid ${(props: any) => props.active ? props.theme.colors.navyD1 : props.theme.colors.greyL1};
+    border: 0.1rem solid
+      ${(props: any) =>
+        props.active ? props.theme.colors.navyD1 : props.theme.colors.greyL1};
+    color: ${(props: any) =>
+      props.active ? props.theme.colors.navyD1 : props.theme.colors.blackL1};
+    border-bottom: ${(props: any) => (props.active ? '.2rem' : '.1rem')} solid
+      ${(props: any) =>
+        props.active ? props.theme.colors.navyD1 : props.theme.colors.greyL1};
   `,
   TabOutLine: styled.button`
     width: 100%;
-    border: .1rem solid ${(props: any) => props.active ? props.theme.colors.navyD1 : props.theme.colors.greyL6};
-    color: ${(props: any) => props.active ? props.theme.colors.navyD1 : props.theme.colors.greyL6};
-   
+    border: 0.1rem solid
+      ${(props: any) =>
+        props.active ? props.theme.colors.navyD1 : props.theme.colors.greyL6};
+    color: ${(props: any) =>
+      props.active ? props.theme.colors.navyD1 : props.theme.colors.greyL6};
   `
 };
 
