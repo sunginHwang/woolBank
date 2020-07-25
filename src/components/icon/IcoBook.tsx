f1a4bb60c7121ae2b0ad9b1f1620704411919e1f
hwangseong-in <gommpo111@gmail.com>
2020-07-25 17:48:14 +0900

    feat-24 : 카테고리 아이콘 정의

---
+++ src/components/icon/IcoBook.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { IIcon } from '../../models/icon/IIcon';
+
+function IcoBook({
+  width = 24,
+  height = 24,
+  fill = 'currentColor'
+}: IIcon) {
+  return (
+    <svg
+      version='1.1' width={width} height={height}
+      viewBox='0 0 24 24'
+    >
+      <path
+        fill={fill}
+        d='M21,4H3A2,2 0 0,0 1,6V19A2,2 0 0,0 3,21H21A2,2 0 0,0 23,19V6A2,2 0 0,0 21,4M3,19V6H11V19H3M21,19H13V6H21V19M14,9.5H20V11H14V9.5M14,12H20V13.5H14V12M14,14.5H20V16H14V14.5Z'
+      />
+
+    </svg>
+  );
+}
+
+export default IcoBook;
