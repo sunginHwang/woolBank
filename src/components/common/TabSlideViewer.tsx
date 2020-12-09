60b330e3734461c1854e97ea4a0b69f8f4a447f4
sunginHwang <gommpo111@gmail.com>
2020-12-09 18:59:58 +0900

    feat-82: 슬라이딩 페이징 구현

--- src/components/common/TabSlideViewer.tsx
+++ src/components/common/TabSlideViewer.tsx
@@ -1,6 +1,6 @@
-import React, { ReactNode, useRef, useState } from 'react';
-import Swiper from 'react-id-swiper';
+import React, { ReactNode, useState } from 'react';
 import styled from 'styled-components';
+import SwipeableViews from 'react-swipeable-views';
 
 import ListWrapper from '@components/common/ListWrapper';
 import Tabs from '@components/common/Tabs';
@@ -17,60 +17,40 @@ export interface ITabSlideViewerProps {
 }
 
 function TabSlideViewer({ tabs, slideViewList, title }: ITabSlideViewerProps) {
-  const [activeTab, setActiveTab] = useState(tabs[0]);
-  const swiperRef = useRef(null);
+  const [ activeTab, setActiveTab ] = useState(tabs[0]);
+  const [ tabIndex, setTavIndex ] = useState(0);
 
   /**
    * 탭 변경 이벤트
    **/
   const onTabChange = (tab: IAssetType) => {
     setActiveTab(tab);
-    onSlideTo(tabs.findIndex((t) => t.type === tab.type));
+    setTavIndex(tabs.findIndex((t) => t.type === tab.type));
   };
 
   /**
    * 리스트 뷰 스와이프
    **/
   const onSlideTo = (index: number) => {
-    if (swiperRef.current === null) {
-      return;
-    }
-    // @ts-ignore
-    swiperRef.current.swiper && swiperRef.current.swiper.slideTo(index);
-  };
-
-  /**
-   * 리스트 뷰 사용자 터치 슬라이드 액션 콜백 이벤트
-   **/
-  const onListSlideChange = (swiper: any) => {
-    setActiveTab(tabs[swiper.realIndex]);
-  };
-
-  // 스와이프 파라미터 정의
-  const SwiperParams = {
-    on: {
-      slideChange: onListSlideChange
-    },
-    containerClass: 'swiper-tab-slide-viewer'
+    setTavIndex(index);
+    setActiveTab(tabs[index]);
   };
 
   return (
     <>
       <S.FixedHeader>
         <S.ListTitle>
-          <p>{title}</p>
+          <p>{title}11</p>
         </S.ListTitle>
         <Tabs tabs={tabs} activeTab={activeTab} onChangeTab={onTabChange} />
       </S.FixedHeader>
-      <PullToRefresh>
-        <ListWrapper>
-          <Swiper ref={swiperRef} {...SwiperParams}>
-            {slideViewList.map((view, index) => (
-              <div key={index}>{view}</div>
-            ))}
-          </Swiper>
-        </ListWrapper>
-      </PullToRefresh>
+      <ListWrapper>
+        <SwipeableViews index={tabIndex} onChangeIndex={onSlideTo} style={{ height: '100%' }}>
+          {slideViewList.map((view, index) => (
+            <S.ListContent key={index}>{view}</S.ListContent>
+          ))}
+        </SwipeableViews>
+      </ListWrapper>
     </>
   );
 }
@@ -80,6 +60,7 @@ export default TabSlideViewer;
 const S: {
   FixedHeader: any;
   ListTitle: any;
+  ListContent: any;
 } = {
   FixedHeader: styled.div`
     position: fixed;
@@ -88,6 +69,9 @@ const S: {
     width: 100%;
     z-index: ${({ theme }) => theme.zIndex.header};
     background-color: ${({ theme }) => theme.colors.white};
+  `,
+  ListContent: styled.div`
+    padding: 2rem 0;
   `,
   ListTitle: styled.div`
     height: 4.8rem;
