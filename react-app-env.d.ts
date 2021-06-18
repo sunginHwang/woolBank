0de5e8bd5a9444794157f993c561b5c3cbda8000
sunginHwang <gommpo111@gmail.com>
2021-06-18 23:35:45 +0900

    feat: 환경 구분 타입 type 선언 처리

---
+++ react-app-env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="node" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly REACT_APP_ENV: 'local' | 'dev' | 'prod';
+  }
+}
