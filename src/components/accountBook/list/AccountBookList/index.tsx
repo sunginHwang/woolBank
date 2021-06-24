1e35163ef90e826701cbeea4d83cd40e5c6418de
sunginHwang <gommpo111@gmail.com>
2021-06-24 18:59:44 +0900

    feat: 가계부 일자 노출 역순 처리

--- src/components/accountBook/list/AccountBookList/index.tsx
+++ src/components/accountBook/list/AccountBookList/index.tsx
@@ -20,33 +20,35 @@ interface IProps {
 function AccountBookList(props: IProps) {
   const { accountBookList } = props;
 
-  const accountBookListGroupDays =
-    _.chain(accountBookList)
-    .groupBy(a => format(a.registerDateTime, 'd'))
+  const accountBookListGroupDays = _.chain(accountBookList)
+    .groupBy((a) => format(a.registerDateTime, 'd'))
     .value();
 
   if (accountBookList.length === 0) {
-    return <S.AccountBookList><EmptyData msg='작성한 소비 내역이 없습니다.' /></S.AccountBookList>;
+    return (
+      <S.AccountBookList>
+        <EmptyData msg='작성한 소비 내역이 없습니다.' />
+      </S.AccountBookList>
+    );
   }
 
   return (
     <S.AccountBookList>
-      {
-        Object.entries(accountBookListGroupDays).map(key => {
-          const days = key[0];
-          const accountBookList = key[1];
-          const totalAmount = getTotalAmount(accountBookList);
-          return (
-            <DayGroup key={days} days={days} totalAmount={totalAmount}>
-              { accountBookList.map(item => <Item key={item.id} accountBookListItem={item} />) }
-            </DayGroup>
-          );
-        })
-      }
+      {Object.entries(accountBookListGroupDays).map((key) => {
+        const days = key[0];
+        const accountBookList = key[1];
+        const totalAmount = getTotalAmount(accountBookList);
+        return (
+          <DayGroup key={days} days={days} totalAmount={totalAmount}>
+            {accountBookList.map((item) => (
+              <Item key={item.id} accountBookListItem={item} />
+            ))}
+          </DayGroup>
+        );
+      }).reverse()}
     </S.AccountBookList>
   );
 }
-
 
 /**
  * 가계부 리스트의 총합금액 구하기 (소비, 지출 포함)
@@ -58,13 +60,12 @@ function getTotalAmount(accountBookList: IAccountBookListItem[]) {
   }, 0);
 }
 
-
 const S: {
   AccountBookList: any;
 } = {
   AccountBookList: styled.div`
     margin-top: 3rem;
-  `,
-}
+  `
+};
 
 export default AccountBookList;
