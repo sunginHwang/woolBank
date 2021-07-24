301da264a6135135dd4b1ddf8588ebcfdffbf478
hwangseong-in <gommpo111@gmail.com>
2021-07-24 17:59:05 +0900

    feat: nova 도입으로 인한 react minor 버저닝 개선 및 일부 import 컨벤션 조정

--- src/components/mypage/UserCardItem.tsx
+++ src/components/mypage/UserCardItem.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import CircleImg from '@components/common/CircleImg';
 import styled from 'styled-components';
+import CircleImg from '@components/common/CircleImg';
 
 interface UserCardItemProps {
   name: string;
