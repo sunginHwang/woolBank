e225f8f2df4ddb3fca753c0243131d17df322777
sunginHwang <gommpo111@gmail.com>
2021-09-29 23:31:14 +0900

    feat: 스토리북 globalStyle 적용 및 input 스토리 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,27 @@
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from '@style/GlobalStyle';
+import theme from '@style/theme';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+      date: /Date$/
+    }
+  }
+};
+
+export const decorators = [
+  (Story) => {
+    return (
+      <>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Story />
+        </ThemeProvider>
+      </>
+    );
+  }
+];
--- src/components/atoms/Input/Input.stories.tsx
+++ src/components/atoms/Input/Input.stories.tsx
@@ -1,16 +1,46 @@
-import Input from './index';
 import { ComponentProps } from 'react';
+import { Meta, Story } from '@storybook/react';
+import Input from './index';
 
-export default {
+const meta: Meta = {
   title: 'Component/atoms/Input',
   component: Input
 };
+export default meta;
 
 type T = ComponentProps<typeof Input>;
-const Template = (args: T) => <Input {...args} />;
+const Template: Story<T> = (args) => <Input {...args} />;
 
-export const Base = Template.bind({});
-//@ts-ignore
+export const Base: Story<T> = Template.bind({});
+
 Base.args = {
   placeholder: '텍스트를 입력해주세요.'
+};
+
+export const InputValue: Story<T> = Template.bind({});
+
+InputValue.args = {
+  value: 'exist inner value'
+};
+
+export const withLabel: Story<T> = Template.bind({});
+
+withLabel.args = {
+  label: '레이블',
+  value: 'exist inner value'
+};
+
+export const withCloseBtn: Story<T> = Template.bind({});
+
+withCloseBtn.args = {
+  isShowCloseBtn: true,
+  value: 'exist inner value'
+};
+
+export const withLabelAndCloseBtn: Story<T> = Template.bind({});
+
+withLabelAndCloseBtn.args = {
+  label: '레이블',
+  isShowCloseBtn: true,
+  value: 'exist inner value'
 };
